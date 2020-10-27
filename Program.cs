1c5cca5e95be14dd8bf5da3e027fedf638e288e1
Eduardo Hurtado <leduardohurtado@estudiante.uniajc.edu.co>
2020-10-27 11:29:15 -0500

    Version 1.0.0

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace learning_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
+            Console.WriteLine("What's your name?");
+
+            int num1 = 12;
+            Console.WriteLine(num1);
+
+            string myString = "99";
+
+            TypeSome();
+            TypeSomeSpec("I'm an argument");
+
+            Console.WriteLine(Int32.Parse(myString) + Int32.Parse(myString));
+            Console.ReadKey();
+        }
+
+        // Access level - (static) - Return type - Method name (params)
+        public static void TypeSome()
+        {
+            Console.WriteLine("Typed some from TypeSome");
+        }
+
+        public static void TypeSomeSpec(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
